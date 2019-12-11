219d07017642ed2305e758aa034de1eececa90cf
Yuvaraj26 <yuvi1064@gmail.com>
2019-12-11 15:04:45 +0530

    New chhange

--- alllbrowser/src/test/java/Drivercheck/Drivermanager.java
+++ alllbrowser/src/test/java/Drivercheck/Drivermanager.java
@@ -1,6 +1,7 @@
 package Drivercheck;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -8,15 +9,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Drivermanager {
 public static void main(String[] args) throws InterruptedException {
-	
+	//Used to invoke safari driver
 	WebDriver driver2=new SafariDriver();
 	driver2.get("https://www.google.com");
 	driver2.quit();
-	
+	//Used to invoke firefox driver
 	WebDriverManager.firefoxdriver().setup();
 	WebDriver driver=new FirefoxDriver();
 	driver.get("https://www.google.com");
 	Thread.sleep(2000);
 	driver.quit();
+	////Used to invoke chrome driver
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver1=new ChromeDriver();
+	driver1.get("https://www.google.com");
+	driver1.quit();
 }
 }
