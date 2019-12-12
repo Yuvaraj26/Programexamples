f9078a3b6868f05148eada50acdefdd56ae18ba2
Yuvaraj26 <yuvi1064@gmail.com>
2019-12-12 15:57:09 +0530

    1

--- alllbrowser/src/test/java/Drivercheck/Actionsexample.java
+++ alllbrowser/src/test/java/Drivercheck/Actionsexample.java
@@ -18,7 +18,8 @@ public class Actionsexample {
 	//a.moveToElement(driver.findElement(By.xpath("//span[contains(@class,'nav-line-2')][contains(text(),'Account & Lists')]"))).build().perform();
 	//Thread.sleep(1000);
 	a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
-Thread.sleep(1000);
+	a.contextClick(driver.findElement(By.xpath("//span[contains(@class,'nav-line-2')][contains(text(),'Account & Lists')]"))).build().perform();
+Thread.sleep(2000);
 	driver.quit();
 
 	}
